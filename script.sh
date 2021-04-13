98f88cd923aca7ad6adfa7457a23df91c23cfdc1
Andrew Selvia <andrew.selvia@sjsu.edu>
2021-04-13 02:06:36 -0700

    Update requirements & script

--- script.sh
+++ script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-#SBATCH --job-name=aselvia
-#SBATCH --output=aselvia.log
+#SBATCH --job-name=l5kit
+#SBATCH --output=l5kit.log
 #SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=3:00:00
-#SBATCH --mail-user=andrew.selvia@sjsu.edu
 #SBATCH --mail-type=END
 module load python3/3.6.6 cuda/10.0
-source cmpe258/l5kit/venv/bin/activate
+source "$ROOT/venv/bin/activate"
 jupyter lab --no-browser --port=10001
